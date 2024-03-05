98fd0fa984016fe691d2f3e613591249b3faed34
✫ Ａ Ｎ Ｅ Ｚ Ａ Ｔ Ｒ Ａ ✫ <129194228+anezatra@users.noreply.github.com>
2024-03-05 12:31:58 +0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,18 @@
+echo
+echo "OSINTERA SETUP"
+echo 
+echo "[*] Installing requirements ..."
+echo
+pip install colorama requests
+apt upgrade && apt-get install nodejs npm
+echo
+echo "[*] Installing API Module ..."
+echo
+npm install -g truecallerjs
+echo
+echo "[*] Please enter credentials"
+echo
+truecallerjs login
+echo
+echo "[+] Done!"
+echo
