b599cb5fd36baf7bbbfe57331a5bf6476b6a99a2
✫ Ａ Ｎ Ｅ Ｚ Ａ Ｔ Ｒ Ａ ✫ <129194228+anezatra@users.noreply.github.com>
2024-03-10 23:10:45 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,5 @@
-echo
-echo "OSINTERA SETUP"
-echo 
-echo "[*] Installing requirements ..."
-echo
 pip install colorama requests
 apt update && apt upgrade -y && apt-get install -y nodejs npm
-echo
-echo "[*] Installing API Module ..."
-echo
 npm install -g truecallerjs
-echo
-echo "[*] Please enter credentials"
-echo
 truecallerjs login
-echo
-echo "[+] Done!"
+
